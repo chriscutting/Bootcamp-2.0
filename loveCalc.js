3b5d4ebdffe54008998387723d9c8af240cea323
chriscutting <34825551+chriscutting@users.noreply.github.com>
2020-01-29 13:50:40 -0500

    Basic JS calculator
    Just dusting off my JS skills

---
+++ loveCalc.js
@@ -0,0 +1,10 @@
+prompt("What is your name?");
+prompt("What is their name?");
+var calcNumber=(Math.floor(Math.random() * 100 + 1));
+console.log(calcNumber);
+if (calcNumber < 30) {
+alert("Oh no, that's a low score, you only got " + calcNumber);
+}
+else {
+alert("Go for it. You scored a " + calcNumber);
+};
