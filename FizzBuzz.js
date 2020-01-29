f96fd2bd93291314eeeaf1d133562b752d096c52
chriscutting <34825551+chriscutting@users.noreply.github.com>
2020-01-29 16:06:43 -0500

    FizzBuzz
    Common answer to an interview question. Also great practice for if/else loops

---
+++ FizzBuzz.js
@@ -0,0 +1,32 @@
+var outPut= [];
+var count = 1;
+function fizzBuzz()
+{
+
+  if (count % 5 === 0 && count % 3 === 0) {
+    outPut.push("FizzBuzz!");
+    count++;
+  }
+
+
+else if (count % 3 === 0) {
+  outPut.push("Fizz");
+  count++;
+}
+
+else if (count % 5 === 0) {
+
+  outPut.push("Buzz");
+  count++;
+}
+
+
+else {
+  outPut.push(count);
+  count++;
+}
+
+
+
+console.log(outPut);
+};
