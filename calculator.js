45a920122e34cda6b7fb6a4bdbf42b3643d501ea
chriscutting <34825551+chriscutting@users.noreply.github.com>
2020-02-27 13:33:56 -0500

    Simplistic Calculator
    Made the world's most basic calculator via the back-end

--- calculator.js
+++ calculator.js
@@ -1,14 +1,24 @@
 //jshint esversion:6
 
-
+const bodyParse = require('body-parser');
 const express = require('express');
 const app = express();
+app.use(bodyParse.urlencoded({extended:true}));
 
 app.listen(3000, function(){
   console.log("server has started on port 3000");
 });
 
+app.post("/", function(req, res){
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("the result of the calculator is " + result);
+});
 
 app.get("/", function(req, res){
-  res.send("I am a calculator!");
+  res.sendFile(__dirname + "/index.html");
 });
