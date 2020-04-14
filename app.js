1aec41b454138cb56b8cbebf75ae63a67bb1c52f
chriscutting <34825551+chriscutting@users.noreply.github.com>
2020-04-14 11:51:21 -0400

    My first independent app
    Building an email grabber using the MailChimp API. Right now it just has basic GET/POST functionality but we'll be adding success/failure pages and stringing it through the API soon enough.

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
+//jshint: esversion 6
+
 const express = require("express");
 const https = require("https");
+const request = require("request");
 const bodyParser = require("body-parser");
-
-
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/", function(req, res){
 
 
 
 res.sendFile(__dirname + "/index.html");
+
 });
 
+
+app.post("/", function(req, res){
+
+var firstName = req.body.fName
+var lastName = req.body.lName
+var email = req.body.email
+
+console.log(firstName, lastName, email);
+
+
+})
 
 
 app.listen(3000, function(){
 
   console.log("Server is spun up on port 3000");
-})
-
-app.post("/", function(req, res){
-
-
-  const unit = "imperial";
-  const apiKey = "c2297fd5bb9a23a3dec934458232920b";
-  const query = req.body.cityName;
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
-
-  https.get(url, function(response){
-    console.log(response.statusCode);
-
-    response.on("data", function(data){
-      const weatherData = JSON.parse(data)
-      const temp = weatherData.main.temp
-      const feelsLike = weatherData.main.feels_like
-      const weatherDescript = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon
-      const imgURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-
-
-
-      res.write("<h1>The temperature in " +  query + " is " + temp + " degrees farenheit!</h1>");
-      res.write("<p>The weather seems like it is " + weatherDescript + "</p>");
-      res.write("<img src = " + imgURL +">");
-      res.send();
-      console.log(temp, feelsLike, weatherDescript);
-});
-});
 });
